a8f092ec7651620ee4da6ec58668c1045df1f9b8
Chris <kaychrisharry@gmail.com>
2025-03-18 04:42:22 +0530

    started with making the project modular

---
+++ main.py
@@ -0,0 +1,16 @@
+def main():
+    """
+    pipe line:
+    1. fetching data from kaggle
+    2. data pre-processing
+    3. making facts and dimensions
+    4. partitioning and clustering
+    5. make kpis
+    6. aggregation tables
+    7. create data marts
+    8. manage analytics and show them on the other pages
+    """
+    pass
+
+if __name__ == '__main__':
+    main()
