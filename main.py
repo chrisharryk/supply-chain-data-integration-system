e53b2031b60777a34c843efb1ea8c023a5894566
Chris <kaychrisharry@gmail.com>
2025-03-18 09:37:10 +0530

    done with EVERYTHING AHHH - except streamlit :(

--- main.py
+++ main.py
@@ -1,33 +1,37 @@
 from modules.etl_pipeline import *
 
 def main():
-    # fetch_kaggle_data('rohitsahoo/sales-forecasting')
-    # df = preprocess_data('./data/train.csv')
-    # df_fact, df_orders, df_shipping, df_customers, df_regions, df_products = create_fact_and_dimensions(df)
-    # tables = {
-    #     'fact_sales': df_fact,
-    #     'dim_orders': df_orders,
-    #     'dim_shipping': df_shipping,
-    #     'dim_customers': df_customers,
-    #     'dim_regions': df_regions,
-    #     'dim_products': df_products
-    # }
+    fetch_kaggle_data('rohitsahoo/sales-forecasting')
+    df = preprocess_data('./data/train.csv')
+    df_fact, df_orders, df_shipping, df_customers, df_regions, df_products = create_fact_and_dimensions(df)
+    tables = {
+        'fact_sales': df_fact,
+        'dim_orders': df_orders,
+        'dim_shipping': df_shipping,
+        'dim_customers': df_customers,
+        'dim_regions': df_regions,
+        'dim_products': df_products
+    }
     # project_id = "learned-spider-453507-p8"
     # dataset_id = "sales_analysis"
-    # push_to_bigquery(tables, project_id, dataset_id)
-    # create_kpi_procedures()
-    # mp = execute_all_kpis()
+    push_to_bigquery(tables)
+    create_kpi_procedures()
+    mp = execute_all_kpis()
     # for k, v in mp.items():
     #     print(v.head())
     # df = execute_kpi_procedure('avg_order_frequency_by_customer', 'kpi_avg_order_frequency_by_customer')
     # print(df.head())
-    # create_aggregation_procedures()
-    # mp = execute_all_aggregations()
+    create_aggregation_procedures()
+    mp = execute_all_aggregations()
     # for k, v in mp.items():
     #     print(v.head())
     #     print()
-    # execute_partitioning_and_clustering()
-    pass
+    execute_partitioning_and_clustering()
+    create_data_marts()
+    # df_inventory = fetch_data_mart("mart_inventory_analysis")
+    # df_order_fulfillment = fetch_data_mart("mart_order_fulfillment")
+    # df_shipping_logistics = fetch_data_mart("mart_shipping_logistics")
+    # print(df_inventory.head(), '\n', df_order_fulfillment.head(), '\n', df_shipping_logistics.head())
 
 if __name__ == '__main__':
     main()
--- modules/etl_pipeline.py
+++ modules/etl_pipeline.py
@@ -4,6 +4,13 @@ import kaggle
 import os
 from google.cloud import bigquery
 import pandas_gbq
+from dotenv import load_dotenv
+
+env_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".env"))
+load_dotenv(env_path)
+
+PROJECT_ID = os.getenv('PROJECT_ID')
+DATASET_ID = os.getenv('DATASET_ID')
 
 client = bigquery.Client()
 
@@ -16,19 +23,6 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger(__name__)
-
-"""
-pipe line:
-1. fetching data from kaggle X
-2. data pre-processing X
-3. making facts and dimensions X
-5. pushing to big query X
-4. partitioning and clustering X
-5. make kpis X
-6. aggregation tables X
-7. create data marts
-8. manage analytics and show them on the other pages
-"""
 
 def fetch_kaggle_data(dataset, download_path = './data'):
     try:
@@ -108,13 +102,13 @@ def create_fact_and_dimensions(df: pd.DataFrame):
         logger.error(f"Error creating fact/dimension tables: {e}")
         return None, None, None, None, None, None
     
-def push_to_bigquery(tables_dict, project_id, dataset_id):
+def push_to_bigquery(tables_dict):
     try:
-        client = bigquery.Client(project=project_id)
+        client = bigquery.Client(project=PROJECT_ID)
         
         for table_name, df in tables_dict.items():
             logger.info(f"Pushing {table_name} to BigQuery...")
-            pandas_gbq.to_gbq(df, f"{dataset_id}.{table_name}", project_id=project_id, if_exists="replace")
+            pandas_gbq.to_gbq(df, f"{DATASET_ID}.{table_name}", project_id=PROJECT_ID, if_exists="replace")
             logger.info(f"Table {table_name} uploaded successfully.")
 
         logger.info("All tables pushed to BigQuery.")
@@ -124,65 +118,65 @@ def push_to_bigquery(tables_dict, project_id, dataset_id):
 
 def create_kpi_procedures():
     queries = {
-        "calculate_lead_time": """CREATE OR REPLACE PROCEDURE sales_analysis.calculate_lead_time()
+        "calculate_lead_time": f"""CREATE OR REPLACE PROCEDURE {DATASET_ID}.calculate_lead_time()
         BEGIN
-            CREATE OR REPLACE TABLE sales_analysis.kpi_lead_time AS
+            CREATE OR REPLACE TABLE {DATASET_ID}.kpi_lead_time AS
             SELECT 
                 f.order_key, 
                 d.`Order ID`, 
                 DATE_DIFF(s.`Ship Date`, d.`Order Date`, DAY) AS lead_time_days
-            FROM sales_analysis.fact_sales f    
-            LEFT JOIN sales_analysis.dim_orders d ON f.order_key = d.order_key
-            LEFT JOIN sales_analysis.dim_shipping s ON f.ship_key = s.ship_key;
+            FROM {DATASET_ID}.fact_sales f    
+            LEFT JOIN {DATASET_ID}.dim_orders d ON f.order_key = d.order_key
+            LEFT JOIN {DATASET_ID}.dim_shipping s ON f.ship_key = s.ship_key;
         END;""",
         
-        "product_category_performance": """CREATE OR REPLACE PROCEDURE sales_analysis.product_category_performance()
+        "product_category_performance": f"""CREATE OR REPLACE PROCEDURE {DATASET_ID}.product_category_performance()
         BEGIN
-            CREATE OR REPLACE TABLE sales_analysis.kpi_product_category_performance AS
+            CREATE OR REPLACE TABLE {DATASET_ID}.kpi_product_category_performance AS
             SELECT 
                 p.Category, 
                 COUNT(f.product_key) AS total_sales,
                 SUM(f.Sales) AS total_revenue,
                 AVG(f.Sales) AS avg_revenue_per_sale
-            FROM sales_analysis.fact_sales f
-            LEFT JOIN sales_analysis.dim_products p ON f.product_key = p.product_key
+            FROM {DATASET_ID}.fact_sales f
+            LEFT JOIN {DATASET_ID}.dim_products p ON f.product_key = p.product_key
             GROUP BY p.Category;
         END;""",
         
-        "product_subcategory_performance": """CREATE OR REPLACE PROCEDURE sales_analysis.product_subcategory_performance()
+        "product_subcategory_performance": f"""CREATE OR REPLACE PROCEDURE {DATASET_ID}.product_subcategory_performance()
         BEGIN
-            CREATE OR REPLACE TABLE sales_analysis.kpi_product_subcategory_performance AS
+            CREATE OR REPLACE TABLE {DATASET_ID}.kpi_product_subcategory_performance AS
             SELECT 
                 p.Category, 
                 p.`Sub-Category`, 
                 COUNT(f.product_key) AS total_sales,
                 SUM(f.Sales) AS total_revenue,
                 AVG(f.Sales) AS avg_revenue_per_sale
-            FROM sales_analysis.fact_sales f
-            LEFT JOIN sales_analysis.dim_products p ON f.product_key = p.product_key
+            FROM {DATASET_ID}.fact_sales f
+            LEFT JOIN {DATASET_ID}.dim_products p ON f.product_key = p.product_key
             GROUP BY p.Category, p.`Sub-Category`;
         END;""",
         
-        "avg_order_value_per_category": """CREATE OR REPLACE PROCEDURE sales_analysis.avg_order_value_per_category()
+        "avg_order_value_per_category": f"""CREATE OR REPLACE PROCEDURE {DATASET_ID}.avg_order_value_per_category()
         BEGIN
-            CREATE OR REPLACE TABLE sales_analysis.kpi_avg_order_value_per_category AS
+            CREATE OR REPLACE TABLE {DATASET_ID}.kpi_avg_order_value_per_category AS
             SELECT 
                 p.Category,
                 AVG(f.Sales) AS avg_order_value
-            FROM sales_analysis.fact_sales f
-            LEFT JOIN sales_analysis.dim_products p ON f.product_key = p.product_key
+            FROM {DATASET_ID}.fact_sales f
+            LEFT JOIN {DATASET_ID}.dim_products p ON f.product_key = p.product_key
             GROUP BY p.Category;
         END;""",
         
-        "avg_order_frequency_by_customer": """CREATE OR REPLACE PROCEDURE sales_analysis.avg_order_frequency_by_customer()
+        "avg_order_frequency_by_customer": f"""CREATE OR REPLACE PROCEDURE {DATASET_ID}.avg_order_frequency_by_customer()
         BEGIN
-            CREATE OR REPLACE TABLE sales_analysis.kpi_avg_order_frequency_by_customer AS
+            CREATE OR REPLACE TABLE {DATASET_ID}.kpi_avg_order_frequency_by_customer AS
             SELECT 
                 c.`Customer Name`,
                 COUNT(DISTINCT f.order_key) / COUNT(DISTINCT d.`Order Date`) AS avg_order_frequency
-            FROM sales_analysis.fact_sales f
-            LEFT JOIN sales_analysis.dim_orders d ON f.order_key = d.order_key
-            LEFT JOIN sales_analysis.dim_customers c ON f.customer_key = c.customer_key
+            FROM {DATASET_ID}.fact_sales f
+            LEFT JOIN {DATASET_ID}.dim_orders d ON f.order_key = d.order_key
+            LEFT JOIN {DATASET_ID}.dim_customers c ON f.customer_key = c.customer_key
             GROUP BY c.`Customer Name`;
         END;"""
     }
@@ -195,14 +189,14 @@ def create_kpi_procedures():
             logging.error(f"Error creating procedure '{proc_name}': {e}")
 
 def execute_kpi_procedure(procedure_name, output_table):
-    query = f"CALL sales_analysis.{procedure_name}();"
+    query = f"CALL {DATASET_ID}.{procedure_name}();"
     
     try:
         logger.info(f"Executing procedure: {procedure_name}...")
         client.query(query).result()
         logger.info(f"Successfully executed procedure: {procedure_name}. Fetching results...")
         
-        df = client.query(f"SELECT * FROM sales_analysis.{output_table}").to_dataframe()
+        df = client.query(f"SELECT * FROM {DATASET_ID}.{output_table}").to_dataframe()
         logger.info(f"Successfully fetched data from {output_table}.")
         return df
     
@@ -230,27 +224,27 @@ def execute_all_kpis():
 
 def create_aggregation_procedures():
     queries = {
-        "aggregate_sales_by_month": """
-        CREATE OR REPLACE PROCEDURE sales_analysis.aggregate_sales_by_month()
+        "aggregate_sales_by_month": f"""
+        CREATE OR REPLACE PROCEDURE {DATASET_ID}.aggregate_sales_by_month()
         BEGIN
-            CREATE OR REPLACE TABLE sales_analysis.agg_sales_monthly AS
+            CREATE OR REPLACE TABLE {DATASET_ID}.agg_sales_monthly AS
             SELECT 
                 EXTRACT(YEAR FROM DATE(d.`Order Date`)) AS order_year,
                 EXTRACT(MONTH FROM DATE(d.`Order Date`)) AS order_month,
                 SUM(f.Sales) AS total_sales,
                 COUNT(DISTINCT f.order_key) AS total_orders
-            FROM sales_analysis.fact_sales f
-            LEFT JOIN sales_analysis.dim_orders d 
+            FROM {DATASET_ID}.fact_sales f
+            LEFT JOIN {DATASET_ID}.dim_orders d 
                 ON f.order_key = d.order_key
             GROUP BY order_year, order_month
             ORDER BY order_year, order_month;
         END;
         """,
         
-        "aggregate_sales_by_product": """
-        CREATE OR REPLACE PROCEDURE sales_analysis.aggregate_sales_by_product()
+        "aggregate_sales_by_product": f"""
+        CREATE OR REPLACE PROCEDURE {DATASET_ID}.aggregate_sales_by_product()
         BEGIN
-            CREATE OR REPLACE TABLE sales_analysis.agg_sales_product AS
+            CREATE OR REPLACE TABLE {DATASET_ID}.agg_sales_product AS
             SELECT 
                 f.product_key,
                 p.`Product Name` AS product_name,  
@@ -258,56 +252,56 @@ def create_aggregation_procedures():
                 p.`Sub-Category` AS sub_category,
                 SUM(f.`Sales`) AS total_sales,
                 COUNT(DISTINCT f.order_key) AS total_orders
-            FROM sales_analysis.fact_sales f
-            LEFT JOIN sales_analysis.dim_products p 
+            FROM {DATASET_ID}.fact_sales f
+            LEFT JOIN {DATASET_ID}.dim_products p 
                 ON f.product_key = p.product_key
             GROUP BY f.product_key, p.`Product Name`, p.`Category`, p.`Sub-Category`
             ORDER BY total_sales DESC;
         END;
         """,
         
-        "aggregate_sales_by_category": """
-        CREATE OR REPLACE PROCEDURE sales_analysis.aggregate_sales_by_category()
+        "aggregate_sales_by_category": f"""
+        CREATE OR REPLACE PROCEDURE {DATASET_ID}.aggregate_sales_by_category()
         BEGIN
-            CREATE OR REPLACE TABLE sales_analysis.agg_sales_category AS
+            CREATE OR REPLACE TABLE {DATASET_ID}.agg_sales_category AS
             SELECT 
                 p.`Category` AS category,
                 SUM(f.`Sales`) AS total_sales,
                 COUNT(DISTINCT f.order_key) AS total_orders
-            FROM sales_analysis.fact_sales f
-            LEFT JOIN sales_analysis.dim_products p 
+            FROM {DATASET_ID}.fact_sales f
+            LEFT JOIN {DATASET_ID}.dim_products p 
                 ON f.product_key = p.product_key
             GROUP BY p.`Category`
             ORDER BY total_sales DESC;
         END;
         """,
         
-        "aggregate_sales_by_subcategory": """
-        CREATE OR REPLACE PROCEDURE sales_analysis.aggregate_sales_by_subcategory()
+        "aggregate_sales_by_subcategory": f"""
+        CREATE OR REPLACE PROCEDURE {DATASET_ID}.aggregate_sales_by_subcategory()
         BEGIN
-            CREATE OR REPLACE TABLE sales_analysis.agg_sales_subcategory AS
+            CREATE OR REPLACE TABLE {DATASET_ID}.agg_sales_subcategory AS
             SELECT 
                 p.`Sub-Category` AS subcategory,
                 SUM(f.`Sales`) AS total_sales,
                 COUNT(DISTINCT f.order_key) AS total_orders
-            FROM sales_analysis.fact_sales f
-            LEFT JOIN sales_analysis.dim_products p 
+            FROM {DATASET_ID}.fact_sales f
+            LEFT JOIN {DATASET_ID}.dim_products p 
                 ON f.product_key = p.product_key
             GROUP BY p.`Sub-Category`
             ORDER BY total_sales DESC;
         END;
         """,
         
-        "aggregate_revenue_by_region": """
-        CREATE OR REPLACE PROCEDURE sales_analysis.aggregate_revenue_by_region()
+        "aggregate_revenue_by_region": f"""
+        CREATE OR REPLACE PROCEDURE {DATASET_ID}.aggregate_revenue_by_region()
         BEGIN
-            CREATE OR REPLACE TABLE sales_analysis.agg_revenue_region AS
+            CREATE OR REPLACE TABLE {DATASET_ID}.agg_revenue_region AS
             SELECT 
                 r.state AS region,
                 SUM(f.`Sales`) AS total_revenue,
                 COUNT(DISTINCT f.order_key) AS total_orders
-            FROM sales_analysis.fact_sales f
-            LEFT JOIN sales_analysis.dim_regions r 
+            FROM {DATASET_ID}.fact_sales f
+            LEFT JOIN {DATASET_ID}.dim_regions r 
                 ON f.region_key = r.region_key
             GROUP BY r.state
             ORDER BY total_revenue DESC;
@@ -323,14 +317,14 @@ def create_aggregation_procedures():
             logging.error(f"Error creating procedure '{procedure_name}': {e}")
 
 def execute_aggregation_procedure(procedure_name, output_table):
-    query = f"CALL sales_analysis.{procedure_name}();"
+    query = f"CALL {DATASET_ID}.{procedure_name}();"
     
     try:
         logger.info(f"Executing procedure: {procedure_name}...")
         client.query(query).result()
         logger.info(f"Successfully executed procedure: {procedure_name}. Fetching results...")
         
-        df = client.query(f"SELECT * FROM sales_analysis.{output_table}").to_dataframe()
+        df = client.query(f"SELECT * FROM {DATASET_ID}.{output_table}").to_dataframe()
         logger.info(f"Successfully fetched data from {output_table}.")
         return df
     
@@ -357,14 +351,14 @@ def execute_all_aggregations():
     return results
 
 def partition_fact_sales():
-    query = """
-    CREATE OR REPLACE TABLE `learned-spider-453507-p8.sales_analysis.fact_sales_partitioned`
+    query = f"""
+    CREATE OR REPLACE TABLE `{PROJECT_ID}.{DATASET_ID}.fact_sales_partitioned`
     PARTITION BY order_date AS
     SELECT 
         f.*, 
         d.`Order Date` AS order_date
-    FROM `learned-spider-453507-p8.sales_analysis.fact_sales` f
-    LEFT JOIN `learned-spider-453507-p8.sales_analysis.dim_orders` d 
+    FROM `{PROJECT_ID}.{DATASET_ID}.fact_sales` f
+    LEFT JOIN `{PROJECT_ID}.{DATASET_ID}.dim_orders` d 
     ON f.order_key = d.order_key;
     """
     try:
@@ -374,16 +368,16 @@ def partition_fact_sales():
         logging.error(f"Error partitioning fact_sales: {e}")
 
 def cluster_fact_sales():
-    query = """
-    CREATE OR REPLACE TABLE `learned-spider-453507-p8.sales_analysis.fact_sales_partitioned_clustered`
+    query = f"""
+    CREATE OR REPLACE TABLE `{PROJECT_ID}.{DATASET_ID}.fact_sales_partitioned_clustered`
     PARTITION BY order_date
     CLUSTER BY customer_key, product_key, region_key
     AS
     SELECT 
       f.*, 
       d.`Order Date` AS order_date
-    FROM `learned-spider-453507-p8.sales_analysis.fact_sales` f
-    JOIN `learned-spider-453507-p8.sales_analysis.dim_orders` d 
+    FROM `{PROJECT_ID}.{DATASET_ID}.fact_sales` f
+    JOIN `{PROJECT_ID}.{DATASET_ID}.dim_orders` d 
     ON f.order_key = d.order_key;
     """
     try:
@@ -395,3 +389,81 @@ def cluster_fact_sales():
 def execute_partitioning_and_clustering():
     partition_fact_sales()
     cluster_fact_sales()
+
+def create_data_marts():
+    queries = {
+        "mart_inventory_analysis": f"""CREATE OR REPLACE TABLE {PROJECT_ID}.{DATASET_ID}.mart_inventory_analysis AS
+        SELECT  
+            p.product_key,  
+            p.`Product Name`,  
+            p.`Category`,  
+            p.`Sub-Category`,  
+            COUNT(DISTINCT f.order_key) AS total_orders,  
+            SUM(f.Sales) AS total_sales_revenue  
+        FROM {PROJECT_ID}.{DATASET_ID}.fact_sales f  
+        JOIN {PROJECT_ID}.{DATASET_ID}.dim_products p  
+            ON f.product_key = p.product_key  
+        GROUP BY  
+            p.product_key, p.`Product Name`, p.`Category`, p.`Sub-Category`;""",
+
+        "mart_order_fulfillment": f"""CREATE OR REPLACE TABLE `{PROJECT_ID}.{DATASET_ID}.mart_order_fulfillment`
+        AS
+        SELECT 
+            f.order_key,
+            d.`Order ID` AS order_id,
+            c.`Customer ID` AS customer_id,
+            c.`Customer Name` AS customer_name,
+            p.`Product Name` AS product_name,
+            r.`Region` AS region_name,  
+            SUM(f.Sales) AS total_sales,
+            COUNT(f.order_key) AS total_orders
+        FROM `{PROJECT_ID}.{DATASET_ID}.fact_sales` f
+        LEFT JOIN `{PROJECT_ID}.{DATASET_ID}.dim_orders` d 
+            ON f.order_key = d.order_key
+        LEFT JOIN `{PROJECT_ID}.{DATASET_ID}.dim_customers` c 
+            ON f.customer_key = c.customer_key
+        LEFT JOIN `{PROJECT_ID}.{DATASET_ID}.dim_products` p 
+            ON f.product_key = p.product_key
+        LEFT JOIN `{PROJECT_ID}.{DATASET_ID}.dim_regions` r 
+            ON f.region_key = r.region_key  
+        GROUP BY f.order_key, order_id, customer_id, customer_name, product_name, region_name;""",
+
+        "mart_shipping_logistics": f"""CREATE OR REPLACE TABLE `{PROJECT_ID}.{DATASET_ID}.mart_shipping_logistics` AS
+        SELECT 
+            f.order_key,
+            d.`Order ID` AS order_id,
+            c.`Customer ID` AS customer_id,
+            c.`Customer Name` AS customer_name,
+            r.`Region` AS region_name,
+            s.`Ship Mode` AS ship_mode,
+            SUM(f.Sales) AS total_sales,
+            COUNT(f.order_key) AS total_orders,
+            CAST(AVG(DATE_DIFF(s.`Ship Date`, d.`Order Date`, DAY)) AS INT) AS avg_shipping_days
+        FROM `{PROJECT_ID}.{DATASET_ID}.fact_sales` f
+        LEFT JOIN `{PROJECT_ID}.{DATASET_ID}.dim_orders` d 
+            ON f.order_key = d.order_key
+        LEFT JOIN `{PROJECT_ID}.{DATASET_ID}.dim_customers` c 
+            ON f.customer_key = c.customer_key
+        LEFT JOIN `{PROJECT_ID}.{DATASET_ID}.dim_regions` r 
+            ON f.region_key = r.region_key
+        LEFT JOIN `{PROJECT_ID}.{DATASET_ID}.dim_shipping` s 
+            ON f.ship_key = s.ship_key
+        GROUP BY f.order_key, order_id, customer_id, customer_name, region_name, ship_mode;"""
+    }
+
+    for mart_name, query in queries.items():
+        try:
+            client.query(query).result()
+            logger.info(f"Data mart '{mart_name}' created successfully.")
+        except Exception as e:
+            logger.error(f"Error creating data mart '{mart_name}': {e}")
+
+def fetch_data_mart(mart_name):
+    try:
+        query = f"SELECT * FROM `{PROJECT_ID}.{DATASET_ID}.{mart_name}`"
+        df = client.query(query).to_dataframe()
+        logger.info(f"Fetched data mart: {mart_name}")
+        return df
+    except Exception as e:
+        logger.error(f"Error fetching data mart '{mart_name}': {e}")
+        return None
